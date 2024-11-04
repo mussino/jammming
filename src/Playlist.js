3bf804e8898f47ae31b76b6a5e0af0fcfa9c0d55
Skalex Stuch <skalexstuch@me.com>
2024-11-04 13:48:08 +0800

    updated Playlist.js

--- src/Playlist.js
+++ src/Playlist.js
@@ -2,27 +2,27 @@ import React, { useCallback } from "react";
 import "./styles/Playlist.css";
 import TrackList from "./Tracklist";
 
-const Playlist = (props) => {
+const Playlist = ({ onNameChange, playlistTracks, onRemove, onSave }) => {
     const handleNameChange = useCallback(
-        (event) => {
-            props.onNameChange(event.target.value);
-        },
-        [props.onNameChange]
+      (event) => {
+        onNameChange(event.target.value);
+      },
+      [onNameChange]
     );
-
+  
     return (
-        <div className="Playlist">
-            <input onChange={handleNameChange} defaultValue={"New Playlist"} />
-            <TrackList
-                tracks={props.playlistTracks}
-                isRemoval={true}
-                onRemove={props.onRemove}
-            />
-            <button className="Playlist-save" onClick={props.onSave}>
-                SAVE TO SPOTIFY
-            </button>
-        </div>
+      <div className="Playlist">
+        <input onChange={handleNameChange} defaultValue={"New Playlist"} />
+        <TrackList
+          tracks={playlistTracks}
+          isRemoval={true}
+          onRemove={onRemove}
+        />
+        <button className="Playlist-save" onClick={onSave}>
+          SAVE TO SPOTIFY
+        </button>
+      </div>
     );
-};
-
-export default Playlist;
+  };
+  
+  export default Playlist;
